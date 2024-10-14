e923c1921c2cc28afaa02406b5acffaeb3e5fc9e
jmr <joshuamrollo@gmail.com>
2024-10-13 20:35:12 -0500

    code for module 1 assignment

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args){
+        // Create variables for first/last name, address, city, zip code for our fictional character
+        String firstName = "John";
+        String lastName = "Doe";
+        String address = "456 Elm Street";
+        String city = "Denver";
+        String zipCode = "12345";
+
+        // print each variable for the fictional character on a new line
+        System.out.println(
+                "Here is the information for the fictional character\n" +
+                "First Name: " + firstName + "\n" +
+                "Last Name: " + lastName + "\n" +
+                "Address: " + address + "\n" +
+                "City: " + city + "\n" +
+                "Zip Code: " + zipCode
+        );
+    }
+}
