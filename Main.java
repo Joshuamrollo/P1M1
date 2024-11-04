516f11fa89eb16babc53467c94da776a860fc827
jmr <joshuamrollo@gmail.com>
2024-11-03 19:41:12 -0600

    new homework

--- Main.java
+++ Main.java
@@ -1,18 +1,9 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        TaxWithholding taxWithholding = new TaxWithholding();
-
-        taxWithholding.calcWeeklyTaxWithholding(300);
-        taxWithholding.calcWeeklyTaxWithholding(1333);
-        taxWithholding.calcWeeklyTaxWithholding(1700);
-        taxWithholding.calcWeeklyTaxWithholding(3442);
-        taxWithholding.calcWeeklyTaxWithholding(500);
-        taxWithholding.calcWeeklyTaxWithholding(1500);
-        taxWithholding.calcWeeklyTaxWithholding(2500);
-        taxWithholding.calcWeeklyTaxWithholding(1);
+        GradeStats statsCalc = new GradeStats();
+        statsCalc.calcStats();
     }
 }
 
